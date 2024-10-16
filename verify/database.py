79850093b1643c328f26b51210acde80b32f5243
ArcasCZ <VydraMan@seznam.cz>
2024-10-16 21:08:06 +0200

    Fix revert

--- verify/database.py
+++ verify/database.py
@@ -205,7 +205,7 @@ class VerifyRule(database.base):
             if role.role_id == role_id:
                 return
 
-        self.roles.append(VerifyRole(role_id=role, guild_id=self.guild_id))
+        self.roles.append(VerifyRole(role_id=role_id, guild_id=self.guild_id))
 
         session.commit()
 
